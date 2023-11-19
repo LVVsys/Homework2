d5ab2db02a954b7bf7706c770203b3531498975c
LVVSys <Driver-DIch@mail.ru>
2023-11-20 01:32:53 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,14 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
         dog=dog+4;
         cat=cat+4;
         paper=paper+4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
         dog=dog-3.5;
         cat=cat-1.6;
         paper=paper-7639;
